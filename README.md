aa3584a5e5bbdf7209d2743874c7b2a9056a950b
Glenn Wilkinson <glenn.wilkinson@gmail.com>
2020-04-06 14:06:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Phish Paste
+Phish Paste is a tiny utility to copy templates, pages, and sending profiles between different [Gophish](https://github.com/gophish/gophish) user accounts.
+
+Example usage:
+```
+$ ./phishpaste --list-users
+[+] Connecting to gophish.db (set $DATABASE_URL to use MySQL)
+[+] Available users: 
+	admin
+	bobby
+```
+ 
+Let's copy all items from `admin` to `bobby`:
+
+```
+$ ./phishpaste --source admin --destination bobby --sending-profiles --email-templates --landing-pages 
+[+] Connecting to gophish.db (set $DATABASE_URL to use MySQL)
+[-] Dry run mode enabled, no data will actually be copied
+[+] Copying 8 Landing Pages from 'admin' to 'bobby'
+	✅ SimpleCreds 
+	✅ HMRC 
+	✅ Update Email Password 
+	✅ Facebook 
+	✅ Employee Evaluation 
+	❌ Dropbox (destination user already has page of the same name) 
+	✅ Antivirus Update 
+	✅ Copy of Update Email Password 
+[+] Copying 9 Email Templates from 'admin' to 'bobby'...
+	✅ Template01 
+	✅ HMRC 
+	✅ Update Email Password 
+	✅ MLTest01 
+	✅ Facebook 
+	✅ Employee Evaluation 
+	❌ Dropbox (destination user already has template of the same name) 
+	✅ Antivirus Update 
+	✅ asriznet-test 
+[+] Copying 9 Sending Profiles from 'admin' to 'bobby'...
+	✅ SendGrid 
+	✅ Mailslurp 
+	✅ HMRC 
+	✅ Update Email Password 
+	✅ Facebook 
+	✅ Employee Evaluation 
+	❌ Dropbox (destination user already has sending profile of the same name) 
+	✅ Antivirus Update 
+	✅ Mailhog 
+
+```
+
+Note: If a destination item exists with the same name it's skipped (as you can see in the above output).
---
+++ phishpaste.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/gophish/gophish/models"
+
+	"github.com/jinzhu/gorm"
+	//_ "github.com/jinzhu/gorm/dialects/mysql" // swap for sqlite below if you want mysql
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+// DB is a global database connection for use by all functions
+var DB *gorm.DB
+
+func main() {
+
+	listUsers := flag.Bool("list-users", false, "List all users")
+
+	copyLandingPages := flag.Bool("landing-pages", false, "Copy Landing Pages from source user to desintation user")
+	copyEmailTemplates := flag.Bool("email-templates", false, "Copy Email Templates from source user to desintation user")
+	copySendingProfiles := flag.Bool("sending-profiles", false, "Copy Sending Profiles from source user to desintation user")
+	dryRun := flag.Bool("dry-run", false, "Don't actually copy the data, just show what would be copied")
+
+	sourceUser := flag.String("source", "", "User to copy data from")
+	desintationUser := flag.String("destination", "", "User to copy data to")
+
+	flag.Parse()
+
+	if !*listUsers && *sourceUser == "" && *desintationUser == "" {
+		fmt.Println("Welcome to Phish Paste. This utility will allow you to copy landing pages, email templates, and sending profiles between Gophish users.")
+		fmt.Println("\nUsage: ")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	//Connect to the database
+	err := initDB()
+	if err != nil {
+		fmt.Println("[!] Error connecting to database: ", err.Error())
+		os.Exit(-1)
+	}
+	defer DB.Close()
+
+	users, err := getUsers() // map from username to id
+	if err != nil {
+		fmt.Println("[!] Error getting users: ", err.Error())
+		os.Exit(-1)
+	}
+
+	if *listUsers {
+
+		fmt.Println("[+] Available users: ")
+		for u := range users {
+			fmt.Printf("\t%s\n", u)
+		}
+
+	} else {
+
+		// Copy each of the requried tables
+		fmt.Println("[-] Dry run mode enabled, no data will actually be copied")
+		if *copyLandingPages {
+			var pagesSource []models.Page
+			var pagesDestination []models.Page
+			pdMap := make(map[string]bool) // Used to check we don't have duplicate named pages
+			var count int
+			DB.Where("user_id = ?", users[*sourceUser]).Find(&pagesSource).Count(&count) // Source
+			DB.Where("user_id = ?", users[*desintationUser]).Find(&pagesDestination)     // Destination
+			for _, pd := range pagesDestination {
+				pdMap[pd.Name] = true
+			}
+
+			fmt.Printf("[+] Copying %d Landing Pages from '%s' to '%s'\n", count, *sourceUser, *desintationUser)
+			for _, p := range pagesSource {
+				if _, ok := pdMap[p.Name]; ok {
+					fmt.Printf("\t❌ %s (destination user already has page of the same name)\n", p.Name)
+				} else {
+					newPage := p
+					newPage.Id = 0
+					newPage.UserId = users[*desintationUser]
+					if !*dryRun {
+						DB.Create(&newPage)
+					}
+					fmt.Printf("\t✅ %s \n", p.Name)
+				}
+			}
+
+		}
+		if *copyEmailTemplates {
+			var templatesSource []models.Template
+			var templatesDestination []models.Template
+			pdMap := make(map[string]bool) // Used to check we don't have duplicate template pages
+			var count int
+			DB.Where("user_id = ?", users[*sourceUser]).Find(&templatesSource).Count(&count) // Source
+			DB.Where("user_id = ?", users[*desintationUser]).Find(&templatesDestination)     // Destination
+			for _, pd := range templatesDestination {
+				pdMap[pd.Name] = true
+			}
+
+			fmt.Printf("[+] Copying %d Email Templates from '%s' to '%s'...\n", count, *sourceUser, *desintationUser)
+			for _, p := range templatesSource {
+				if _, ok := pdMap[p.Name]; ok {
+					fmt.Printf("\t❌ %s (destination user already has template of the same name)\n", p.Name)
+				} else {
+					newTemplate := p
+					newTemplate.Id = 0
+					newTemplate.UserId = users[*desintationUser]
+					if !*dryRun {
+						DB.Create(&newTemplate)
+					}
+					fmt.Printf("\t✅ %s \n", p.Name)
+				}
+			}
+
+		}
+		if *copySendingProfiles {
+			var smtpsSource []models.SMTP
+			var smtpsDestination []models.SMTP
+			pdMap := make(map[string]bool) // Used to check we don't have duplicate template pages
+			var count int
+			DB.Where("user_id = ?", users[*sourceUser]).Find(&smtpsSource).Count(&count) // Source
+			DB.Where("user_id = ?", users[*desintationUser]).Find(&smtpsDestination)     // Destination
+			for _, pd := range smtpsDestination {
+				pdMap[pd.Name] = true
+			}
+
+			fmt.Printf("[+] Copying %d Sending Profiles from '%s' to '%s'...\n", count, *sourceUser, *desintationUser)
+			for _, p := range smtpsSource {
+				if _, ok := pdMap[p.Name]; ok {
+					fmt.Printf("\t❌ %s (destination user already has sending profile of the same name)\n", p.Name)
+				} else {
+					newSMTP := p
+					newSMTP.Id = 0
+					newSMTP.UserId = users[*desintationUser]
+					if !*dryRun {
+						DB.Create(&newSMTP)
+					}
+					fmt.Printf("\t✅ %s \n", p.Name)
+				}
+			}
+
+		}
+
+	}
+
+}
+
+// initDB setups the database connection
+func initDB() error {
+
+	var err error
+	if os.Getenv("DATABASE_URL") == "" {
+		fmt.Println("[+] Connecting to gophish.db (set $DATABASE_URL to use MySQL)")
+		DB, err = gorm.Open("sqlite3", "gophish.db")
+	} else {
+		fmt.Println("[+] Connecting to datbase")
+		DB, err = gorm.Open("mysql", os.Getenv("DATABASE_URL"))
+	}
+
+	return err
+}
+
+func getUsers() (map[string]int64, error) {
+
+	userMap := make(map[string]int64)
+	var users []models.User
+	if err := DB.Find(&users).Error; err != nil {
+		return userMap, err
+	}
+
+	for _, u := range users {
+		userMap[u.Username] = u.Id
+	}
+
+	return userMap, nil
+}
